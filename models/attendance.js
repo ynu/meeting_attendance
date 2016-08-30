3286dd7ea133124ad5832478d0d2421572b25feb
liudonghua123 <liudonghua123@gmail.com>
2016-08-30 20:51:56 +0800

    initial commit

---
+++ models/attendance.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+    var attendance = sequelize.define("attendance", {
+        name: DataTypes.STRING,
+        phone_number: DataTypes.STRING,
+        department: DataTypes.STRING,
+        job: DataTypes.STRING
+    }, {
+        classMethods: {
+            // associate: function(models) {
+            //     User.hasMany(models.Task)
+            // }
+        }
+    });
+
+    return attendance;
+};
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var models  = require('../models');
+
+/* GET meeting attendance page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'customization' });
+});
+
+/* GET meeting attendance page. */
+router.post('/', function(req, res, next) {
+  var name = req.body.name;
+  var phone_number = req.body.phone_number;
+  var department = req.body.department;
+  var job = req.body.job;
+
+  models.attendance.create({
+    name: name,
+    phone_number: phone_number,
+    department: department,
+    job: job
+  }).then(function(user) {
+    res.render('attend_success', { title: 'customization' });
+  }).catch(function(err) {
+    console.log(err);
+    res.render('attend_failed', { title: 'customization' });
+  }).finally(function() {
+    // finally gets called always regardless of
+    // whether the promises resolved with or without errors.
+    // however this handler does receive any arguments.
+  });
+
+});
+
+module.exports = router;
